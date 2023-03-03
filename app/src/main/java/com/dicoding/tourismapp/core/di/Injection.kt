fd47cb2c173c04a4c32f77cb3f4d7902adaa0452
bayu07750 <bayu07750@gmail.com>
2023-03-03 22:02:29 +0700

    codelab retrofit

--- app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -7,16 +7,19 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiConfig
 import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.JsonHelper
 
 object Injection {
-    fun provideRepository(context: Context): TourismRepository {
+    private fun provideRepository(context: Context): TourismRepository {
         val database = TourismDatabase.getInstance(context)
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+
+        val apiService = ApiConfig.provideApiService()
+        val remoteDataSource = RemoteDataSource.getInstance(apiService)
         val localDataSource = LocalDataSource.getInstance(database.tourismDao())
         val appExecutors = AppExecutors()
 
